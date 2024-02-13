92e233c70198223b2e2ea642ef3e9650f755723c
Paras Pathania <paras.pathania@flexigroup.com.au>
2024-02-13 13:15:18 +1100

    Added 176. Second Highest Salary

---
+++ SQL/LeetCode/176. Second Highest Salary.sql
@@ -0,0 +1,69 @@
+/*
+---------------------------- LEETCODE CHALLENGE -------------------------------------
+----------------------------------- SQL 50 ------------------------------------------
+
+176. Second Highest Salary
+
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+
+*/
+
+
+------------------- Solution -------------------------------
+/* Write your T-SQL query statement below */
+WITH CTE as
+(
+select a.salary from(
+    select salary, dense_rank() over(order by salary DESC) as rn from employee 
+    ) a where a.rn = 2
+)
+select (select top 1 * from CTE) AS SecondHighestSalary; 
